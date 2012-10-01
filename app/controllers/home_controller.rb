59ee029ffca0b9c0c7a427ca6c1f188f7a2ed4d6
Richard Larcombe <rjlarcombe@gmail.com>
2012-10-01 09:03:11 -0700

    Oops

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -28,7 +28,7 @@ class HomeController < ApplicationController
   def give
     @selected_nav = "give"
     @our_heart = Page.find("give-our-heart")
-    @chloe_and_stu = Page.find("give-chloe-and-stu-s-orphanage")
+    @chloe_and_stu = Page.find("give-chloe-and-stu-s-kids-homes")
     @erica = Page.find("give-erica-greve")
     @john_and_sarah = Page.find("give-john-and-sarah-s-orphanage")    
   end  
